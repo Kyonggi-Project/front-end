c9a69f895762cacdae00d957d108a3d369f09ebf
gergese <jeffsong16@gmail.com>
2024-03-04 10:55:58 +0900

    라우팅 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 import Signup from './components/Signup';
+import { RouterProvider, createBrowserRouter, Outlet } from 'react-router-dom';
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Outlet />,
+      children: [
+        {
+          path: '',
+          element: <p>Home</p>,
+        },
+        {
+          path: 'signup',
+          element: <Signup />,
+        },
+        {
+          path: 'login',
+          element: <p>Login Page</p>,
+        }
+      ],
+    },
+  ]);
   return (
-    <>
-    <Signup />
-    </>
+    <RouterProvider router={router} />
   );
 }
 
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,28 +1,43 @@
+import { Link, useNavigate } from "react-router-dom";
 import "./Signup.css";
 
 function Signup() {
-    return (
-        <div className="signup-form">
-            <div>
-                <h1 className="signup-title">Create an account</h1>
-                <p className="option">Choose one of the option to go</p>
-                <div>
-                    <input type="text" placeholder="Name" className="input-box" />
-                </div>
-                <div>
-                    <input type="text" placeholder="Nickname" className="input-box" />
-                </div>
-                <div>
-                    <input type="email" placeholder="Email" className="input-box" />
-                </div>
-                <div>
-                    <input type="password" placeholder="Password" className="input-box" />
-                </div>
-                <p className="a-text">Already have an account? <a href='/login' className="login-link">Login</a></p>
-                <button className="button">Signup</button>
-            </div>
+
+  const navigate = useNavigate();
+
+  function handleSubmit() {
+    //양식 벡엔드로 전달
+
+    navigate('/');
+  }
+
+  return (
+    <form className="signup-form" onSubmit={handleSubmit}>
+      <div>
+        <h1 className="signup-title">Create an account</h1>
+        <p className="option">Choose one of the option to go</p>
+        <div>
+          <input type="text" placeholder="Name" className="input-box" required />
         </div>
-    );
+        <div>
+          <input type="text" placeholder="Nickname" className="input-box" required />
+        </div>
+        <div>
+          <input type="email" placeholder="Email" className="input-box" required />
+        </div>
+        <div>
+          <input type="password" placeholder="Password" className="input-box" required />
+        </div>
+        <p className="a-text">
+          Already have an account?
+          <Link to="/login"className="login-link">Login</Link>
+        </p>
+        <button className="button" type="submit">
+          Signup
+        </button>
+      </div>
+    </form>
+  );
 }
 
 export default Signup;
