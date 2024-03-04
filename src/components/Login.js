3e52e149d7a3c095afe3f59a2a390ab6cf0b42d6
qowotjr <110812746+qowotjr@users.noreply.github.com>
2024-03-04 19:17:18 +0900

    Add files via upload

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,68 @@
 import React from 'react';
 import './Login.css';
+import { useState } from 'react';
+import axios from "axios";
 
 export default function Login(){
+    const [enteredValues, setEnteredValues] = useState({
+        email: '',
+        password: '',
+    });
+    
+    function handleSubmit(event){
+        event.preventDefault();
+        
+        console.log(enteredValues);
+        axios.post('https://localhost:3000/login', enteredValues).then((res)=>{console.log(res.data)})
+    }
+
+    function handlelInputChange(identifier, value) {
+        setEnteredValues(prevValues => ({
+          ...prevValues,
+          [identifier]: value
+        }))
+      }
+
+    function handleGoogleLogin(){
+        const googleAuthUrl = 'https://accounts.google.com/o/oauth2/v2/auth?' + new URLSearchParams({
+            response_type: 'token',
+            client_id: 'YOUR_CLIENT_ID', // Google 개발자 콘솔에서 생성한 클라이언트 ID
+            redirect_uri: window.location.origin, // 로그인 후 리다이렉트될 URL
+            scope: 'profile email', // 요청할 권한(scope) 설정
+        });
+
+        // Google OAuth 페이지로 리다이렉트
+        window.location.href = googleAuthUrl;
+    }
+
     return(
         <div className='rectangle-1'>
             <div className='rectangle-2'>
             <h1 className="login-title">Login to your account</h1>
             <p className='option-text'>enter email and password</p>
-            <div>
-                <input type="email" placeholder="email" className="input-field"/>
-            </div>
-            <div>
-                <input type="password" placeholder="password" className="input-field"/>
-            </div>
-            <p className='additional-text'>Don't you have a account? <a href='/signup'>Sign up</a></p>
-            <button className="login-button">Login</button>
+            <form>
+                <div>
+                    <input 
+                        type="email" 
+                        placeholder="email" 
+                        className="input-field"
+                        onChange={(event) => handlelInputChange('email', event.target.value)}
+	                    value={enteredValues.email}
+                    />
+                </div>
+                <div>
+                    <input 
+                        type="password" 
+                        placeholder="password" 
+                        className="input-field"
+                        onChange={(event) => handlelInputChange('password', event.target.value)}
+	                    value={enteredValues.password}
+                    />
+                </div>
+                <p className='additional-text'>Don't you have a account? <a href='/signup'>Sign up</a></p>
+                <button className='oauth-button' onClick={handleGoogleLogin}><img src='../google.png' width='30px' alt='google'/></button>
+                <button className="login-button" onClick={handleSubmit}>Login</button>
+            </form>
             </div>
         </div>
     );
