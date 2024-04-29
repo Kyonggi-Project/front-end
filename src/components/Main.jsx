9e9dbc25b688ddc2ff0c07e2d5c27569d2e0b318
qowotjr <110812746+qowotjr@users.noreply.github.com>
2024-04-29 16:51:14 +0900

    Main 페이지 장르별 리스트 버튼

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,17 +13,20 @@ import "./Main.css"
 const imagePaths = [Pamyo, Spyfamily, Dune, Malo, Mukspark, Concert, Whatslove];
 const matchedData = imagePaths.map((imagePath, index) => ({
   imagePath: imagePath,
-  data: JsonData[index], // 이미지 순서에 맞는 JSON 데이터를 가져옴
+  data: JsonData.movie[index], // 이미지 순서에 맞는 JSON 데이터를 가져옴
 }));
 
 function Main() {
   const [startIndex, setStartIndex] = useState(0); // 보여질 이미지 수 상태
   const [showIndex, setShowIndex] = useState(4);
-
-  const [recstartIndex, setRecstartIndex] = useState(0);
-  const [recshowIndex, setRecshowIndex] = useState(0);
-
   const [showModal, setShowModal] = useState(false);
+
+  const [selectedGenre, setSelectedGenre] = useState("인기");
+
+  // 장르별 순위 변화 함수
+  const handleGenreClick = (genre) => {
+    setSelectedGenre(genre);
+  };
 
   // 모달 표시 함수
   const handleModalClick = () => {
@@ -40,16 +43,12 @@ function Main() {
     const screenWidth = window.innerWidth;
     if (screenWidth > 1600) {
       setShowIndex(4);
-      setRecshowIndex(6);
     } else if (screenWidth > 1100) {
       setShowIndex(3);
-      setRecshowIndex(4);
     } else if (screenWidth > 900) {
       setShowIndex(2);
-      setRecshowIndex(2);
     } else {
       setShowIndex(1);
-      setRecshowIndex(1);
     }
   };
 
@@ -72,16 +71,6 @@ function Main() {
     setStartIndex(newStartIndex >= imagePaths.length ? 0 : newStartIndex);
   };
 
-  const handleRecPrevButtonClick = () => {
-    const newStartIndex = recstartIndex - recshowIndex;
-    setRecstartIndex(newStartIndex >= imagePaths.length ? 0 : newStartIndex);
-  };
-
-  const handleRecNextButtonClick = () => {
-    const newStartIndex = recstartIndex + recshowIndex; // 4개씩 보여주므로 4을 더해줍니다.
-    setRecstartIndex(newStartIndex >= imagePaths.length ? 0 : newStartIndex);
-  };
-
   return (
     <>
       <div className="main-title-page">
@@ -93,10 +82,21 @@ function Main() {
       </div>
       <div className="main-gallery-container">
         <div className="main-image-list-container">
-          <h1 className="main-h1-name">Ranking</h1>
+        <ul className="main-button-list">
+          <li><button className={`button ${selectedGenre === "인기" ? "main-selected" : ""}`} onClick={() => handleGenreClick("인기")}>인기</button></li>
+          <li><button className={`button ${selectedGenre === "액션" ? "main-selected" : ""}`} onClick={() => handleGenreClick("액션")}>액션</button></li>
+          <li><button className={`button ${selectedGenre === "공포" ? "main-selected" : ""}`} onClick={() => handleGenreClick("공포")}>공포</button></li>
+          <li><button className={`button ${selectedGenre === "가족" ? "main-selected" : ""}`} onClick={() => handleGenreClick("가족")}>가족</button></li>
+          <li><button className={`button ${selectedGenre === "드라마" ? "main-selected" : ""}`} onClick={() => handleGenreClick("드라마")}>드라마</button></li>
+          <li><button className={`button ${selectedGenre === "코미디" ? "main-selected" : ""}`} onClick={() => handleGenreClick("코미디")}>코미디</button></li>
+          <li><button className={`button ${selectedGenre === "SF" ? "main-selected" : ""}`} onClick={() => handleGenreClick("SF")}>SF</button></li>
+          <li><button className={`button ${selectedGenre === "로맨스" ? "main-selected" : ""}`} onClick={() => handleGenreClick("로맨스")}>로맨스</button></li>
+          <li><button className={`button ${selectedGenre === "다큐" ? "main-selected" : ""}`} onClick={() => handleGenreClick("다큐")}>다큐</button></li>
+        </ul>
+          <h1 className="main-h1-name">{selectedGenre === "인기" ? "이번주 인기작 Top 10" : `이번주 ${selectedGenre} Top 10`}</h1>
           <ul className="main-image-list">
             {/* 이미지 배열을 map 함수를 사용하여 동적으로 렌더링 */}
-            {matchedData
+            {(selectedGenre === "인기" ? matchedData : filterByGenre(matchedData, selectedGenre))
               .slice(startIndex, startIndex + showIndex)
               .map((image, index) => (
                 <li key={index}>
@@ -129,49 +129,6 @@ function Main() {
           <button
             className="main-scroll-button next"
             onClick={handleNextButtonClick}
-          >
-            →
-          </button>
-        </div>
-      </div>
-
-      <div className="main-gallery-container">
-        <div className="main-image-list-container">
-          <h2 className="main-h2-name">회원님에게 추천하는 작품</h2>
-          <ul className="main-image-list">
-            {/* 이미지 배열을 map 함수를 사용하여 동적으로 렌더링 */}
-            {matchedData
-              .slice(recstartIndex, recstartIndex + recshowIndex)
-              .map((image, index) => (
-                <li key={index}>
-                  <a href="#">
-                    <div className="main-image-wrapper1">
-                      <a href={`/details?index=${index}`}>
-                        <img
-                          src={image.imagePath}
-                          alt={`Image ${recstartIndex + index + 1}`}
-                        />
-                      </a>
-                    </div>
-                    <div className="main-data-info">{image.data.title}</div>
-                    <div className="main-data-count">
-                      {image.data.date} - {image.data.count}만명
-                    </div>
-                  </a>
-                </li>
-              ))}
-          </ul>
-          <button
-            className={`main-scroll-button prev ${
-              recstartIndex === 0 ? "h1" : ""
-            }`}
-            onClick={handleRecPrevButtonClick}
-          >
-            ←
-          </button>
-          <button
-            className="main-scroll-button next"
-            onClick={handleRecNextButtonClick}
           >
             →
           </button>
