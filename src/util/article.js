0072b62f5a3ad1b9d444b6db9ce7c37c58e72d39
gergese <jeffsong16@gmail.com>
2024-04-30 21:24:14 +0900

    articles.js 수정

--- src/util/article.js
+++ src/util/article.js
@@ -56,7 +56,11 @@ export function httpRequest2(method, url, body, success, fail) {
       if (response.status === 200 || response.status === 201) {
         return success(response);
       }
-      if (response.status === 401) {
+
+    })
+    .catch((error) => {
+      if (error.response.status === 401) {
+        console.log('error');
         axios.post(host + '/api/token/createToken', {
           withCredentials: true,
         }, {
@@ -67,14 +71,14 @@ export function httpRequest2(method, url, body, success, fail) {
         })
           .then(res => {
             if (res.status === 200) {
+              console.log(res.data)
               localStorage.setItem('access_token', res.data.accessToken);
               httpRequest2(method, url, body, success, fail);
             }
           })
           .catch((error) => fail(error));
       } else {
-        return fail();
+        return fail(error);
       }
-    })
-    .catch((error) => fail(error));
+    });
 }
